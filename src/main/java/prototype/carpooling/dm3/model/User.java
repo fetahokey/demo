53cbcfad239f2f73232f169ad922186f91c0a09b
fetahokey <ouldkradda.fateh@gmail.com>
2019-10-02 22:30:29 +0100

    //TODO

--- src/main/java/prototype/carpooling/dm3/model/User.java
+++ src/main/java/prototype/carpooling/dm3/model/User.java
@@ -21,6 +21,7 @@ public class User {
     private String email;
     private boolean enabled;
 
+    //TODO
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles",
