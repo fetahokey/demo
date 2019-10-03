2d3f28ab85ab50575384b1374c12f24065cfc20b
Bessem Zitouni <bessemzitouni@gmail.com>
2019-10-03 17:16:50 +0100

    add getter & setters to model

--- src/main/java/prototype/carpooling/dm3/model/Role.java
+++ src/main/java/prototype/carpooling/dm3/model/Role.java
@@ -1,10 +1,10 @@
 package prototype.carpooling.dm3.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "role")
@@ -18,7 +18,27 @@ public class Role {
     private String role;
     private String description;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getRole() {
+        return role;
+    }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/prototype/carpooling/dm3/model/User.java
+++ src/main/java/prototype/carpooling/dm3/model/User.java
@@ -3,6 +3,7 @@ package prototype.carpooling.dm3.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -35,7 +36,56 @@ public class User {
         this.id = user.getId();
         this.username = user.getUsername();
         this.password = user.getPassword();
+        this.email = user.getEmail();
         this.enabled = user.isEnabled();
         this.roles = user.getRoles();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 }
