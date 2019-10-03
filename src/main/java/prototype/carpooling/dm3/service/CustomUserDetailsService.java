e00d30801391a33fc90eb713cb6900293724d6f3
Bessem Zitouni <bessemzitouni@gmail.com>
2019-10-03 17:15:42 +0100

    clean customUserDetailsService code

--- src/main/java/prototype/carpooling/dm3/service/CustomUserDetailsService.java
+++ src/main/java/prototype/carpooling/dm3/service/CustomUserDetailsService.java
@@ -1,26 +1,24 @@
 package prototype.carpooling.dm3.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import prototype.carpooling.dm3.model.CustomUserDetail;
-import prototype.carpooling.dm3.model.User;
 import prototype.carpooling.dm3.repository.UserRepository;
-import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> optionalUser = userRepository.findByUsername(username);
-        optionalUser.orElseThrow(() -> new UsernameNotFoundException("User not found !"));
-        CustomUserDetail customUserDetail = optionalUser.map(user -> {return new CustomUserDetail(user);}).get();
-//        CustomUserDetail customUserDetail2 = optionalUser.map(CustomUserDetail::new).get(); // Lambda
-        return customUserDetail;
+        return userRepository.findByUsername(username)
+                .map(CustomUserDetail::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found !"));
     }
 }
